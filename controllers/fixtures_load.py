ef4de4b0028b15b2eaa167bbe6e5e4bdb512c4ea
Baubry2-iut90 <baptiste.aubry02@edu.univ-fcomte.fr>
2026-02-01 14:23:55 +0100

    Modification du Init pour la bdd
    Refactor SQL drop table commands to individual execute statements.

--- controllers/fixtures_load.py
+++ controllers/fixtures_load.py
@@ -11,15 +11,14 @@ fixtures_load = Blueprint('fixtures_load', __name__,
 @fixtures_load.route('/base/init')
 def fct_fixtures_load():
     mycursor = get_db().cursor()
-    mycursor.execute('''
-    DROP TABLE IF EXISTS ligne_commande;
-    DROP TABLE IF EXISTS ligne_panier;
-    DROP TABLE IF EXISTS commande;
-    DROP TABLE IF EXISTS jeux_video;
-    DROP TABLE IF EXISTS utilisateur;
-    DROP TABLE IF EXISTS etat;
-    DROP TABLE IF EXISTS type_jeux_video;
-    DROP TABLE IF EXISTS console;''')
+    mycursor.execute("DROP TABLE IF EXISTS ligne_commande;")
+    mycursor.execute("DROP TABLE IF EXISTS ligne_panier;")
+    mycursor.execute("DROP TABLE IF EXISTS commande;")
+    mycursor.execute("DROP TABLE IF EXISTS jeux_video;")
+    mycursor.execute("DROP TABLE IF EXISTS utilisateur;")
+    mycursor.execute("DROP TABLE IF EXISTS etat;")
+    mycursor.execute("DROP TABLE IF EXISTS type_jeux_video;")
+    mycursor.execute("DROP TABLE IF EXISTS console;")
 
     mycursor.execute('''
     CREATE TABLE IF NOT EXISTS type_jeux_video (

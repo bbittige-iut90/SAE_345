71bd327e81ca779eb5652d3dbec289f72180216d
Baptiste Aubry <baptiste.aubry02@edu.univ-fcomte.fr>
2026-02-26 16:43:15 +0100

    bug client_commande

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -35,8 +35,18 @@ def client_commande_valide():
 @client_commande.route('/client/commande/add', methods=['POST'])
 def client_commande_add():
     mycursor = get_db().cursor()
+    id_client = session['id_user']
 
     # choix de(s) (l')adresse(s)
+    # 1. Récupérer les articles du panier de l'utilisateur
+    sql = """
+        SELECT jv.id_jeux_video, jv.prix_jeux_video, lp.quantite
+        FROM ligne_panier AS lp
+        JOIN jeux_video AS jv ON lp.jeux_video_id = jv.id_jeux_video
+        WHERE lp.utilisateur_id = %s;
+    """
+    mycursor.execute(sql, (id_client,))
+    items_ligne_panier = mycursor.fetchall()
 
     id_client = session['id_user']
     sql = ''' selection du contenu du panier de l'utilisateur '''
@@ -46,18 +56,36 @@ def client_commande_add():
     #     return redirect('/client/article/show')
                                            # https://pynative.com/python-mysql-transaction-management-using-commit-rollback/
     #a = datetime.strptime('my date', "%b %d %Y %H:%M")
+    # 2. Vérifier si le panier est vide
+    if not items_ligne_panier:
+        flash('Votre panier est vide, impossible de passer une commande.', 'alert-warning')
+        return redirect('/client/article/show')
 
     sql = ''' creation de la commande '''
+    # 3. Créer une nouvelle commande dans la table "commande"
+    # On considère que l'état 1 correspond à "En cours de traitement"
+    sql_create_commande = "INSERT INTO commande(date_commande, utilisateur_id, etat_id) VALUES (NOW(), %s, 1);"
+    mycursor.execute(sql_create_commande, (id_client,))
+    id_commande = mycursor.lastrowid
 
     sql = '''SELECT last_insert_id() as last_insert_id'''
     # numéro de la dernière commande
+    # 4. Pour chaque article du panier, l'ajouter à "ligne_commande"
     for item in items_ligne_panier:
         sql = ''' suppression d'une ligne de panier '''
         sql = "  ajout d'une ligne de commande'"
+        sql_insert_ligne = "INSERT INTO ligne_commande(commande_id, jeux_video_id, prix, quantite) VALUES (%s, %s, %s, %s);"
+        mycursor.execute(sql_insert_ligne, (id_commande, item['id_jeux_video'], item['prix_jeux_video'], item['quantite']))
+
+    # 5. Vider le panier de l'utilisateur
+    sql_delete_panier = "DELETE FROM ligne_panier WHERE utilisateur_id = %s;"
+    mycursor.execute(sql_delete_panier, (id_client,))
 
     get_db().commit()
     flash(u'Commande ajoutée','alert-success')
     return redirect('/client/article/show')
+    flash('Votre commande a bien été enregistrée.', 'alert-success')
+    return redirect('/client/commande/show')
 
 
 

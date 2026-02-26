6a3fe27502803e517b9705135fc1f9cc2bb4cf27
Baubry2-iut90 <baptiste.aubry02@edu.univ-fcomte.fr>
2026-02-26 16:17:48 +0100

    Change environment variable for database user

--- connexion_db.py
+++ connexion_db.py
@@ -11,7 +11,7 @@ def get_db():
     if db is None:
         db = g._database = pymysql.connect(
             host=os.getenv("HOST"),                 # à modifier
-            user=os.getenv("USER"),                 # à modifier
+            user=os.getenv("BD_USER"),                 # à modifier
             password=os.getenv("PASSWORD"),         # à modifier
             database=os.getenv("DATABASE"),         # à modifier
             charset="utf8mb4",

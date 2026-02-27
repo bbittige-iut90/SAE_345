2efd077f4b3bc966161a2835078c8f966e8096c7
Bryan Bittiger <bryan.bittiger@edu.univ-fcomte.fr>
2026-02-27 13:53:17 +0100

    admin article

--- controllers/admin_article.py
+++ controllers/admin_article.py
@@ -1,13 +1,6 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
-import math
-import os.path
-from random import random
-
-from flask import Blueprint
-from flask import request, render_template, redirect, flash
-#from werkzeug.utils import secure_filename
-
+from flask import Blueprint, request, render_template, redirect, flash
 from connexion_db import get_db
 
 admin_article = Blueprint('admin_article', __name__,
@@ -17,7 +10,11 @@ admin_article = Blueprint('admin_article', __name__,
 @admin_article.route('/admin/article/show')
 def show_article():
     mycursor = get_db().cursor()
-    sql = '''  requête admin_article_1
+    sql = '''
+    SELECT id_jeux_video AS id_article, nom_jeux_video AS nom, prix_jeux_video AS prix, stock, photo_jeux_video AS image, libelle_type_jeux_video AS libelle, type_jeux_video_id AS type_article_id
+    FROM jeux_video
+    JOIN type_jeux_video ON jeux_video.type_jeux_video_id = type_jeux_video.id_type_jeux_video
+    ORDER BY nom; 
     '''
     mycursor.execute(sql)
     articles = mycursor.fetchall()
@@ -27,145 +24,89 @@ def show_article():
 @admin_article.route('/admin/article/add', methods=['GET'])
 def add_article():
     mycursor = get_db().cursor()
-
-    return render_template('admin/article/add_article.html'
-                           #,types_article=type_article,
-                           #,couleurs=colors
-                           #,tailles=tailles
-                            )
+    mycursor.execute("SELECT id_type_jeux_video as id_article, libelle_type_jeux_video as libelleType FROM type_jeux_video;")
+    types_article = mycursor.fetchall()
+    mycursor.execute("SELECT id_console as id, libelle_console as libelle FROM console;")
+    consoles = mycursor.fetchall()
+    return render_template('admin/article/add_article.html', types_article=types_article, consoles=consoles)
 
 
 @admin_article.route('/admin/article/add', methods=['POST'])
 def valid_add_article():
-    mycursor = get_db().cursor()
-
     nom = request.form.get('nom', '')
-    type_article_id = request.form.get('type_article_id', '')
+    type_id = request.form.get('type_article_id', '')
+    console_id = request.form.get('console_id', '')
     prix = request.form.get('prix', '')
+    stock = request.form.get('stock', '')
     description = request.form.get('description', '')
-    image = request.files.get('image', '')
+    photo = request.form.get('photo', '')
 
-    if image:
-        filename = 'img_upload'+ str(int(2147483647 * random())) + '.png'
-        image.save(os.path.join('static/images/', filename))
-    else:
-        print("erreur")
-        filename=None
-
-    sql = '''  requête admin_article_2 '''
-
-    tuple_add = (nom, filename, prix, type_article_id, description)
-    print(tuple_add)
-    mycursor.execute(sql, tuple_add)
+    message = 'Ajout d\'un article -- nom: ' + nom
+    mycursor = get_db().cursor()
+    tuple_param = (nom, description, prix, photo, stock, type_id, console_id)
+    sql = '''
+    INSERT INTO jeux_video (nom_jeux_video, description, prix_jeux_video, photo_jeux_video, stock, type_jeux_video_id, console_id)
+    VALUES (%s, %s, %s, %s, %s, %s, %s); 
+    '''
+    mycursor.execute(sql, tuple_param)
     get_db().commit()
-
-    print(u'article ajouté , nom: ', nom, ' - type_article:', type_article_id, ' - prix:', prix,
-          ' - description:', description, ' - image:', image)
-    message = u'article ajouté , nom:' + nom + '- type_article:' + type_article_id + ' - prix:' + prix + ' - description:' + description + ' - image:' + str(
-        image)
     flash(message, 'alert-success')
     return redirect('/admin/article/show')
 
 
-@admin_article.route('/admin/article/delete', methods=['GET'])
+@admin_article.route('/admin/article/delete')
 def delete_article():
-    id_article=request.args.get('id_article')
+    id_article = request.args.get('id', 0)
     mycursor = get_db().cursor()
-    sql = ''' requête admin_article_3 '''
-    mycursor.execute(sql, id_article)
-    nb_declinaison = mycursor.fetchone()
-    if nb_declinaison['nb_declinaison'] > 0:
-        message= u'il y a des declinaisons dans cet article : vous ne pouvez pas le supprimer'
-        flash(message, 'alert-warning')
-    else:
-        sql = ''' requête admin_article_4 '''
-        mycursor.execute(sql, id_article)
-        article = mycursor.fetchone()
-        print(article)
-        image = article['image']
-
-        sql = ''' requête admin_article_5  '''
-        mycursor.execute(sql, id_article)
-        get_db().commit()
-        if image != None:
-            os.remove('static/images/' + image)
-
-        print("un article supprimé, id :", id_article)
-        message = u'un article supprimé, id : ' + id_article
-        flash(message, 'alert-success')
-
+    tuple_param = (id_article,)
+    sql = '''DELETE FROM jeux_video WHERE id_jeux_video = %s;'''
+    mycursor.execute(sql, tuple_param)
+    get_db().commit()
+    flash('Suppression d\'un article -- id: ' + str(id_article), 'alert-warning')
     return redirect('/admin/article/show')
 
 
 @admin_article.route('/admin/article/edit', methods=['GET'])
 def edit_article():
-    id_article=request.args.get('id_article')
+    id_article = request.args.get('id', 0)
     mycursor = get_db().cursor()
     sql = '''
-    requête admin_article_6    
+    SELECT id_jeux_video AS id_article, nom_jeux_video AS nom, description, prix_jeux_video AS prix,stock, photo_jeux_video AS image, type_jeux_video_id, console_id
+    FROM jeux_video
+    WHERE id_jeux_video = %s;
     '''
-    mycursor.execute(sql, id_article)
+    mycursor.execute(sql, (id_article,))
     article = mycursor.fetchone()
-    print(article)
-    sql = '''
-    requête admin_article_7
-    '''
-    mycursor.execute(sql)
+    mycursor.execute("SELECT id_type_jeux_video as id, libelle_type_jeux_video as libelleType FROM type_jeux_video;")
     types_article = mycursor.fetchall()
-
-    # sql = '''
-    # requête admin_article_6
-    # '''
-    # mycursor.execute(sql, id_article)
-    # declinaisons_article = mycursor.fetchall()
-
-    return render_template('admin/article/edit_article.html'
-                           ,article=article
-                           ,types_article=types_article
-                         #  ,declinaisons_article=declinaisons_article
-                           )
+    mycursor.execute("SELECT id_console as id, libelle_console as libelle FROM console;")
+    consoles = mycursor.fetchall()
+    return render_template('admin/article/edit_article.html', article=article, types_article=types_article, consoles=consoles)
 
 
 @admin_article.route('/admin/article/edit', methods=['POST'])
 def valid_edit_article():
-    mycursor = get_db().cursor()
-    nom = request.form.get('nom')
-    id_article = request.form.get('id_article')
-    image = request.files.get('image', '')
-    type_article_id = request.form.get('type_article_id', '')
+    id_article = request.form.get('id')
+    nom = request.form.get('nom', '')
+    type_id = request.form.get('type_article_id', '')
+    console_id = request.form.get('console_id', '')
     prix = request.form.get('prix', '')
-    description = request.form.get('description')
+    stock = request.form.get('stock', '')
+    description = request.form.get('description', '')
+    photo = request.form.get('photo', '')
+
+    message = 'Modification d\'un article -- nom: ' + nom
+    mycursor = get_db().cursor()
+    tuple_param = (nom, description, prix, photo, stock, type_id, console_id, id_article)
     sql = '''
-       requête admin_article_8
-       '''
-    mycursor.execute(sql, id_article)
-    image_nom = mycursor.fetchone()
-    image_nom = image_nom['image']
-    if image:
-        if image_nom != "" and image_nom is not None and os.path.exists(
-                os.path.join(os.getcwd() + "/static/images/", image_nom)):
-            os.remove(os.path.join(os.getcwd() + "/static/images/", image_nom))
-        # filename = secure_filename(image.filename)
-        if image:
-            filename = 'img_upload_' + str(int(2147483647 * random())) + '.png'
-            image.save(os.path.join('static/images/', filename))
-            image_nom = filename
-
-    sql = '''  requête admin_article_9 '''
-    mycursor.execute(sql, (nom, image_nom, prix, type_article_id, description, id_article))
-
+    UPDATE jeux_video
+    SET nom_jeux_video  = %s, description = %s, prix_jeux_video = %s, photo_jeux_video = %s,stock = %s, type_jeux_video_id = %s, console_id = %s
+    WHERE id_jeux_video = %s; 
+    '''
+    mycursor.execute(sql, tuple_param)
     get_db().commit()
-    if image_nom is None:
-        image_nom = ''
-    message = u'article modifié , nom:' + nom + '- type_article :' + type_article_id + ' - prix:' + prix  + ' - image:' + image_nom + ' - description: ' + description
     flash(message, 'alert-success')
     return redirect('/admin/article/show')
-
-
-
-
-
-
 
 @admin_article.route('/admin/article/avis/<int:id>', methods=['GET'])
 def admin_avis(id):
